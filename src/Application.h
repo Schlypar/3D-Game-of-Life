4087cc150910e1e38e8f51d7c2d4358780a174d2
Schlypar <schlypar@gmail.com>
2024-05-08 13:59:58 +0300

    get imgui working (not properly)

--- src/Application.h
+++ src/Application.h
@@ -1,9 +1,5 @@
 #pragma once
 
-// #include <imgui/imgui.h>
-// #include <imgui/imgui_impl_glfw.h>
-// #include <imgui/imgui_impl_opengl3.h>
-
 #include "Window.h"
 
 #include "IndexBuffer.h"
@@ -38,6 +34,38 @@ public:
             unsigned int height = 1080
     )
         : window(Window::Settings(title, width, height)) {
+        // Decide GL+GLSL versions
+#if defined(IMGUI_IMPL_OPENGL_ES2)
+        // GL ES 2.0 + GLSL 100
+        const char* glsl_version = "#version 100";
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_ES_API);
+#elif defined(__APPLE__)
+        // GL 3.2 + GLSL 150
+        const char* glsl_version = "#version 150";
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // 3.2+ only
+        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);           // Required on Mac
+#else
+        // GL 3.0 + GLSL 130
+        const char* glsl_version = "#version 130";
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+        //glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
+        //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // 3.0+ only
+#endif
+
+        // Setup Dear ImGui context
+        IMGUI_CHECKVERSION();
+        ImGui::CreateContext();
+        // Setup Dear ImGui style
+        ImGui::StyleColorsDark();
+
+        // Setup Platform/Renderer backends
+        ImGui_ImplGlfw_InitForOpenGL(window, true);
+        ImGui_ImplOpenGL3_Init(glsl_version);
     }
 
     void ConfigureCallbacks(
@@ -54,39 +82,87 @@ public:
     }
 
     void Run() {
-        Renderer renderer;
-        Cursor3DRenderer cursor_renderer;
+        // Renderer renderer;
+        // Cursor3DRenderer cursor_renderer;
 
-        Shader shader = { "resources/shaders/test.shader" };
-        shader.Bind();
+        // Shader shader = { "resources/shaders/test.shader" };
+        // shader.Bind();
 
-        auto cube = Cube(glm::vec3 { 0.0f }, glm::vec3 { 0.0f }, 0.5f);
-        cube.BindIndices();
+        // auto cube = Cube(glm::vec3 { 0.0f }, glm::vec3 { 0.0f }, 0.5f);
+        // cube.BindIndices();
 
-        auto cursor = Cursor3D();
-        cursor.BindIndices();
+        // auto cursor = Cursor3D();
+        // cursor.BindIndices();
 
         // auto prism = Prism(glm::vec3 { 1.0f, 1.0f, 0.0f }, glm::vec3 { 0.0f, 0.0f, 0.0f }, 0.5f);
         // prism.BindIndices();
 
-        IndexBuffer::Init();
+        // IndexBuffer::Init();
+
+        ImGuiIO& io = ImGui::GetIO();
+        (void) io;
+        // io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard; // Enable Keyboard Controls
+        // io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;  // Enable Gamepad Controls
+
+        bool show_demo_window = true;
+        bool show_another_window = false;
+        ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
 
         while (!window.ShouldClose()) {
+            ImGui_ImplOpenGL3_NewFrame();
+            ImGui_ImplGlfw_NewFrame();
+            ImGui::NewFrame();
+            if (show_demo_window) {
+                ImGui::ShowDemoWindow(&show_demo_window);
+            }
+            // 2. Show a simple window that we create ourselves. We use a Begin/End pair to create a named window.
+            {
+                static float f = 0.0f;
+                static int counter = 0;
+
+                ImGui::Begin("Hello, world!"); // Create a window called "Hello, world!" and append into it.
+
+                ImGui::Text("This is some useful text.");          // Display some text (you can use a format strings too)
+                ImGui::Checkbox("Demo Window", &show_demo_window); // Edit bools storing our window open/close state
+                ImGui::Checkbox("Another Window", &show_another_window);
+
+                ImGui::SliderFloat("float", &f, 0.0f, 1.0f);             // Edit 1 float using a slider from 0.0f to 1.0f
+                ImGui::ColorEdit3("clear color", (float*) &clear_color); // Edit 3 floats representing a color
+
+                if (ImGui::Button("Button")) { // Buttons return true when clicked (most widgets return true when edited/activated)
+                    counter++;
+                }
+                ImGui::SameLine();
+                ImGui::Text("counter = %d", counter);
+
+                ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / io.Framerate, io.Framerate);
+                ImGui::End();
+            }
+
             Parameters::currentFrameTime = static_cast<float>(glfwGetTime());
             Parameters::deltaTime = Parameters::currentFrameTime - Parameters::lastFrameTime;
             Parameters::lastFrameTime = Parameters::currentFrameTime;
 
-            renderer.Clear();
+            // renderer.Clear();
 
-            renderer.Draw<Cube>(cube, Parameters::camera, shader);
-            // renderer.Draw<Prism>(prism, Parameters::camera, shader);
-            cursor.SetScaleFactor(100);
-            renderer.Draw<Cursor3D>(cursor, Parameters::camera, shader);
-            cursor.SetScaleFactor(0.1);
-            cursor_renderer.Draw<Cursor3D>(cursor, Parameters::camera, shader);
+            // renderer.Draw<Cube>(cube, Parameters::camera, shader);
+            // // renderer.Draw<Prism>(prism, Parameters::camera, shader);
+            // cursor.SetScaleFactor(100);
+            // renderer.Draw<Cursor3D>(cursor, Parameters::camera, shader);
+            // cursor.SetScaleFactor(0.1);
+            // cursor_renderer.Draw<Cursor3D>(cursor, Parameters::camera, shader);
 
-            window.OnUpdate(Parameters::camera);
-            ProcessInput();
+            // window.OnUpdate(Parameters::camera);
+            // ProcessInput();
+            ImGui::Render();
+            int display_w, display_h;
+            glfwGetFramebufferSize(window, &display_w, &display_h);
+            glViewport(0, 0, display_w, display_h);
+            glClearColor(clear_color.x * clear_color.w, clear_color.y * clear_color.w, clear_color.z * clear_color.w, clear_color.w);
+            glClear(GL_COLOR_BUFFER_BIT);
+            ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+            glfwSwapBuffers(window);
         }
         glfwTerminate();
         exit(EXIT_SUCCESS);
