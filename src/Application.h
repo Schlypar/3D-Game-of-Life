ddaabcbba141dc0126de37f8dc82ca6f63a32f84
Schlypar <schlypar@gmail.com>
2024-05-10 20:48:34 +0300

    tested that events are being correctly 'caught'

--- src/Application.h
+++ src/Application.h
@@ -1,15 +1,21 @@
 #pragma once
 
-#include "Window.h"
-
-#include "IndexBuffer.h"
-
 #include "Models/Cube.h"
 #include "Models/Cursor3D.h"
 #include "Models/Prism.h"
-#include "renderer/Renderer.h"
+#include "Renderer.h"
+#include "Window.h"
+#include "events/MouseEvent.h"
+
+#include <imgui/imgui.h>
+#include <imgui/imgui_impl_glfw.h>
+#include <imgui/imgui_impl_opengl3.h>
+
+#include <iostream>
 
 namespace GoL {
+
+static bool captureMouse = true;
 
 class Application {
 public:
@@ -33,52 +39,12 @@ public:
             unsigned int width = 1980,
             unsigned int height = 1080
     )
-        : window(Window::Settings(title, width, height)) {
-        // Decide GL+GLSL versions
-#if defined(IMGUI_IMPL_OPENGL_ES2)
-        // GL ES 2.0 + GLSL 100
-        const char* glsl_version = "#version 100";
-        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
-        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
-        glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_ES_API);
-#elif defined(__APPLE__)
-        // GL 3.2 + GLSL 150
-        const char* glsl_version = "#version 150";
-        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
-        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // 3.2+ only
-        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);           // Required on Mac
-#else
-        // GL 3.0 + GLSL 130
-        const char* glsl_version = "#version 130";
-        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
-        //glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
-        //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // 3.0+ only
-#endif
-
-        // Setup Dear ImGui context
-        IMGUI_CHECKVERSION();
-        ImGui::CreateContext();
-        // Setup Dear ImGui style
-        ImGui::StyleColorsDark();
-
-        // Setup Platform/Renderer backends
-        ImGui_ImplGlfw_InitForOpenGL(window, true);
-        ImGui_ImplOpenGL3_Init(glsl_version);
-    }
-
-    void ConfigureCallbacks(
-            void (*error_callback)(int, const char*),
-            void (*mouse_callback)(GLFWwindow*, double, double)
-    ) {
-        // window settings
-        window.SetVSync(true);
-        window.Configure();
-        window.SetErrorCallback(error_callback);
-        window.SetCursorPosCallback(mouse_callback);
-
-        // possibly other settings...
+        : window(Window::Data(title, width, height)) {
+        this->window.SetEventCallback(
+                [this](Event& e) {
+                    this->OnEvent(e);
+                }
+        );
     }
 
     void Run() {
@@ -99,46 +65,7 @@ public:
 
         // IndexBuffer::Init();
 
-        ImGuiIO& io = ImGui::GetIO();
-        (void) io;
-        // io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard; // Enable Keyboard Controls
-        // io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;  // Enable Gamepad Controls
-
-        bool show_demo_window = true;
-        bool show_another_window = false;
-        ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
-
         while (!window.ShouldClose()) {
-            ImGui_ImplOpenGL3_NewFrame();
-            ImGui_ImplGlfw_NewFrame();
-            ImGui::NewFrame();
-            if (show_demo_window) {
-                ImGui::ShowDemoWindow(&show_demo_window);
-            }
-            // 2. Show a simple window that we create ourselves. We use a Begin/End pair to create a named window.
-            {
-                static float f = 0.0f;
-                static int counter = 0;
-
-                ImGui::Begin("Hello, world!"); // Create a window called "Hello, world!" and append into it.
-
-                ImGui::Text("This is some useful text.");          // Display some text (you can use a format strings too)
-                ImGui::Checkbox("Demo Window", &show_demo_window); // Edit bools storing our window open/close state
-                ImGui::Checkbox("Another Window", &show_another_window);
-
-                ImGui::SliderFloat("float", &f, 0.0f, 1.0f);             // Edit 1 float using a slider from 0.0f to 1.0f
-                ImGui::ColorEdit3("clear color", (float*) &clear_color); // Edit 3 floats representing a color
-
-                if (ImGui::Button("Button")) { // Buttons return true when clicked (most widgets return true when edited/activated)
-                    counter++;
-                }
-                ImGui::SameLine();
-                ImGui::Text("counter = %d", counter);
-
-                ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / io.Framerate, io.Framerate);
-                ImGui::End();
-            }
-
             Parameters::currentFrameTime = static_cast<float>(glfwGetTime());
             Parameters::deltaTime = Parameters::currentFrameTime - Parameters::lastFrameTime;
             Parameters::lastFrameTime = Parameters::currentFrameTime;
@@ -152,70 +79,16 @@ public:
             // cursor.SetScaleFactor(0.1);
             // cursor_renderer.Draw<Cursor3D>(cursor, Parameters::camera, shader);
 
-            // window.OnUpdate(Parameters::camera);
-            // ProcessInput();
-            ImGui::Render();
-            int display_w, display_h;
-            glfwGetFramebufferSize(window, &display_w, &display_h);
-            glViewport(0, 0, display_w, display_h);
-            glClearColor(clear_color.x * clear_color.w, clear_color.y * clear_color.w, clear_color.z * clear_color.w, clear_color.w);
-            glClear(GL_COLOR_BUFFER_BIT);
-            ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-
-            glfwSwapBuffers(window);
+            window.OnUpdate();
         }
+
         glfwTerminate();
         exit(EXIT_SUCCESS);
     }
 
-private:
-    void ProcessInput() {
-        float cameraSpeed = static_cast<float>(2.5 * Parameters::deltaTime);
-        if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) {
-            Parameters::camera.ProcessKeyboard(GoL::CameraMovement::FORWARD, Parameters::deltaTime);
-        }
-        if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) {
-            Parameters::camera.ProcessKeyboard(GoL::CameraMovement::BACKWARD, Parameters::deltaTime);
-        }
-        if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS) {
-            Parameters::camera.ProcessKeyboard(GoL::CameraMovement::LEFT, Parameters::deltaTime);
-        }
-        if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) {
-            Parameters::camera.ProcessKeyboard(GoL::CameraMovement::RIGHT, Parameters::deltaTime);
-        }
-        if (glfwGetKey(window, GLFW_KEY_LEFT_SHIFT) == GLFW_PRESS) {
-            Parameters::camera.ProcessKeyboard(GoL::CameraMovement::UP, Parameters::deltaTime);
-        }
-        if (glfwGetKey(window, GLFW_KEY_LEFT_CONTROL) == GLFW_PRESS) {
-            Parameters::camera.ProcessKeyboard(GoL::CameraMovement::DOWN, Parameters::deltaTime);
-        }
-        if (glfwGetKey(window, GLFW_KEY_Q) == GLFW_PRESS) {
-            Parameters::camera.ProcessMouseMovement(-10.0f, 0.0f);
-        }
-        if (glfwGetKey(window, GLFW_KEY_E) == GLFW_PRESS) {
-            Parameters::camera.ProcessMouseMovement(10.0f, 0.0f);
-        }
-        if (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS) {
-            Parameters::camera.ProcessMouseMovement(-10.0f, 0.0f);
-        }
-        if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS) {
-            Parameters::camera.ProcessMouseMovement(10.0f, 0.0f);
-        }
-        if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS) {
-            Parameters::camera.ProcessMouseMovement(0.0f, 10.0f);
-        }
-        if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS) {
-            Parameters::camera.ProcessMouseMovement(0.0f, -10.0f);
-        }
-        if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
-            glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
-            glfwSetCursorPosCallback(window, NULL);
-            Parameters::firstMouse = false;
-        }
-        // if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_1 == GLFW_PRESS)) {
-        //     glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
-        //     glfwSetCursorPosCallback(window, mouse_callback);
-        // }
+    void OnEvent(Event& e) {
+        std::cout << e << std::endl;
+        // EventDispatcher dispatcher(e);
     }
 };
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,9 @@
 #include <stdio.h>
 
 #include "Application.h"
-
-static void error_callback(int error, const char* description);
-static void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+#include "imgui/imgui_impl_glfw.h"
 
 int main() {
     GoL::Application application;
-    application.ConfigureCallbacks(error_callback, mouse_callback);
     application.Run();
-}
-
-void error_callback(int error, const char* description) {
-    fprintf(stderr, "Error: %s\n", description);
-}
-
-void mouse_callback(GLFWwindow* window, double xpos, double ypos) {
-    if (GoL::Application::Parameters::firstMouse) {
-        GoL::Application::Parameters::lastX = xpos;
-        GoL::Application::Parameters::lastY = ypos;
-        GoL::Application::Parameters::firstMouse = false;
-    }
-
-    glm::vec2 offset(xpos - GoL::Application::Parameters::lastX, GoL::Application::Parameters::lastY - ypos);
-    GoL::Application::Parameters::lastX = xpos;
-    GoL::Application::Parameters::lastY = ypos;
-
-    GoL::Application::Parameters::camera.ProcessMouseMovement(offset.x, offset.y);
 }
