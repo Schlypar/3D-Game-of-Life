730ba4019f6ec402c57cd0f9a9145b3339d46d4c
Schlypar <schlypar@gmail.com>
2024-05-10 23:02:27 +0300

    added layers (not tested and not used at this point)

---
+++ src/core/Layer.cpp
@@ -0,0 +1,9 @@
+#include "Layer.h"
+
+namespace GoL {
+
+Layer::Layer(const std::string& label)
+    : label(label) {
+}
+
+}
---
+++ src/core/LayerStack.h
@@ -0,0 +1,54 @@
+#pragma once
+
+#include "Layer.h"
+
+namespace GoL {
+
+class LayerStack {
+private:
+    std::vector<Layer*> layers;
+    unsigned int layerInsertIndex = 0;
+
+public:
+    LayerStack() = default;
+    ~LayerStack();
+
+    void PushLayer(Layer* layer);
+    void PushOverlay(Layer* overlay);
+    void PopLayer(Layer* layer);
+    void PopOverlay(Layer* overlay);
+
+    std::vector<Layer*>::iterator begin() {
+        return layers.begin();
+    }
+
+    std::vector<Layer*>::iterator end() {
+        return layers.end();
+    }
+
+    std::vector<Layer*>::reverse_iterator rbegin() {
+        return layers.rbegin();
+    }
+
+    std::vector<Layer*>::reverse_iterator rend() {
+        return layers.rend();
+    }
+
+    std::vector<Layer*>::const_iterator begin() const {
+        return layers.begin();
+    }
+
+    std::vector<Layer*>::const_iterator end() const {
+        return layers.end();
+    }
+
+    std::vector<Layer*>::const_reverse_iterator rbegin() const {
+        return layers.rbegin();
+    }
+
+    std::vector<Layer*>::const_reverse_iterator rend() const {
+        return layers.rend();
+    }
+};
+
+}
