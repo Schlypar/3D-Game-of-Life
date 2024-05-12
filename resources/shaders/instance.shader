bad55fdfaa238e628b7faa8f1fa67a7ad6773e6c
B-den <dbancre@gmail.com>
2024-05-12 11:57:12 +0300

    change example a bit

--- resources/shaders/instance.shader
+++ resources/shaders/instance.shader
@@ -1,8 +1,8 @@
 #shader vertex
 #version 330 core
 
-layout (location = 0) in vec4 position;
-layout (location = 1) in vec4 color;
+layout (location = 0) in vec3 position;
+layout (location = 1) in vec3 color;
 layout (location = 2) in mat4 instanceMatrix;
 out vec4 i_color;
 
@@ -10,8 +10,8 @@ uniform mat4 ProjectionView;
 
 void main()
 {
-    i_color = color;
-    gl_Position = ProjectionView * instanceMatrix * position;
+    i_color = vec4(color.x, color.y, color.z, 0.0);
+    gl_Position = ProjectionView * instanceMatrix * vec4(position.x, position.y, position.z, 1.0);
 };
 
 #shader fragment
--- src/Application.h
+++ src/Application.h
@@ -8,6 +8,7 @@
 // #include "Models/Vertex.h"
 // #include "VertexBuffer.h"
 // #include "VertexBufferLayout.h"
+#include "Shader.h"
 #include "Window.h"
 
 #include "IndexBuffer.h"
@@ -16,6 +17,9 @@
 // #include "Models/Cursor3D.h"
 #include "Models/Cursor3D_no_indices.h"
 // #include "Models/Prism.h"
+#include "glad/gl.h"
+#include "glm/ext/matrix_float4x4.hpp"
+#include "glm/ext/matrix_transform.hpp"
 #include "glm/fwd.hpp"
 #include "renderer/Renderer.h"
 #include <cstring>
@@ -26,8 +30,7 @@ namespace GoL {
 
 // static GLuint VBO;
 // static GLuint MBO;
-#define MATRICES_LOCATION 1
-#define NUM_INSTANCES 1000
+#define MATRICES_LOCATION 2
 
 // static void CreateVertexBuffer() {
 //     float vertices[9] = { -0.3f, -0.3f, 0.0f, 0.3f, -0.3f, 0.0f, 0.0f, 0.25f, 0.0f };
@@ -113,18 +116,23 @@ static void CompileShaders() {
 
     vs = "#version 330 core\n"
          "layout (location = 0) in vec3 Vertex;\n"
-         "layout (location = 1) in mat4 Matrix;\n"
+         "layout (location = 1) in vec3 color;"
+         "layout (location = 2) in mat4 Matrix;\n"
+         "uniform mat4 ProjectionView;\n"
+         "out vec4 i_color;\n"
          "void main()\n"
          "{\n"
-         "  gl_Position = Matrix*vec4(Vertex.x, Vertex.y, Vertex.z, 1.0);\n"
+         "  i_color = vec4(color.x, color.y, color.z, 0.0);\n"
+         "  gl_Position = ProjectionView*Matrix*vec4(Vertex.x, Vertex.y, Vertex.z, 1.0);\n"
          "}\n";
     AddShader(ShaderProgram, vs.c_str(), GL_VERTEX_SHADER);
 
     fs = "#version 330 core\n"
+         "in vec4 i_color;\n"
          "out vec4 FragColor;\n"
          "void main()\n"
          "{\n"
-         "  FragColor = vec4(1.0, 1.0, 1.0, 0.0);\n"
+         "  FragColor = i_color;\n"
          "}\n";
     AddShader(ShaderProgram, fs.c_str(), GL_FRAGMENT_SHADER);
 
@@ -189,44 +197,61 @@ public:
         // possibly other settings...
     }
 
-    void Run() {
-        GLclampf Red = 0.2f, Green = 0.2f, Blue = 0.2f, Alpha = 0.0f;
-        glClearColor(Red, Green, Blue, Alpha);
+    GLuint setupExample() {
+        GLuint VBO, MBO, VAO;
+        // float vertices[18] = {
+        //     -0.3f, -0.3f, 0.0f, 1.0f, 0.0f, 0.0f,
+        //     0.3f, -0.3f, 0.0f, 1.0f, 0.0f, 0.0f,
+        //     0.0f, 0.25f, 0.0f, 1.0f, 0.0f, 0.0f,};
+            
+        float vertices[36] = {
+            0.0f, 0.0f, 0.0f, 1, 0, 0, // center
+            1.0f, 0.0f, 0.0f, 1, 0, 0, // abscissa
+            0.0f, 0.0f, 0.0f, 0, 1, 0, // center
+            0.0f, 1.0f, 0.0f, 0, 1, 0, // ordinate
+            0.0f, 0.0f, 0.0f, 0, 0, 1, // center
+            0.0f, 0.0f, 1.0f, 0, 0, 1  // applicata
+        };
+        int vert_size = 36 * sizeof(float);
 
-        // CreateVertexBuffer();
+        glGenVertexArrays(1, &VAO);
+        glBindVertexArray(VAO);
 
-        GLuint VBO;
-        GLuint MBO;
-        float vertices[9] = { -0.3f, -0.3f, 0.0f, 0.3f, -0.3f, 0.0f, 0.0f, 0.25f, 0.0f };
         glGenBuffers(1, &VBO);
         glBindBuffer(GL_ARRAY_BUFFER, VBO);
-        glBufferData(GL_ARRAY_BUFFER, 9 * sizeof(float), vertices, GL_STATIC_DRAW);
-        glVertexAttribPointer(VERTEX_LOCATION, 3, GL_FLOAT, GL_FALSE, (GLsizei) 3 * sizeof(float), NULL);
+        glBufferData(GL_ARRAY_BUFFER, vert_size, vertices, GL_STATIC_DRAW);
+        glVertexAttribPointer(VERTEX_LOCATION, 3, GL_FLOAT, GL_FALSE, (GLsizei) 6 * sizeof(float), NULL);
         glEnableVertexAttribArray(VERTEX_LOCATION);
+        glVertexAttribPointer(VERTEX_LOCATION + 1, 3, GL_FLOAT, GL_FALSE, (GLsizei) 6 * sizeof(float), (void*)(3 * sizeof(float)));
+        glEnableVertexAttribArray(VERTEX_LOCATION + 1);
 
-        float mat[NUM_INSTANCES * 16];
-        for (int matrix_id = 0; matrix_id < NUM_INSTANCES; matrix_id++) {
-            float pos_x = 0.002f * matrix_id * cos(40 * M_PI * matrix_id / NUM_INSTANCES);
-            float pos_y = 0.002f * matrix_id * sin(40 * M_PI * matrix_id / NUM_INSTANCES);
-            float scale = 0.0004f * matrix_id;
-            int i = 16 * matrix_id;
-            mat[i + 0] = scale;
-            mat[i + 4] = 0.0f;
-            mat[i + 8] = 0.0f;
-            mat[i + 12] = pos_x;
-            mat[i + 1] = 0.0f;
-            mat[i + 5] = scale;
-            mat[i + 9] = 0.0f;
-            mat[i + 13] = pos_y;
-            mat[i + 2] = 0.0f;
-            mat[i + 6] = 0.0f;
-            mat[i + 10] = scale;
-            mat[i + 14] = 0.0f;
-            mat[i + 3] = 0.0f;
-            mat[i + 7] = 0.0f;
-            mat[i + 11] = 0.0f;
-            mat[i + 15] = 1.0f;
-        }
+        int NUM_INSTANCES = 2;
+        // float mat[NUM_INSTANCES * 16];
+        // for (int matrix_id = 0; matrix_id < NUM_INSTANCES; matrix_id++) {
+        //     float pos_x = 0.002f * matrix_id * cos(40 * M_PI * matrix_id / NUM_INSTANCES);
+        //     float pos_y = 0.002f * matrix_id * sin(40 * M_PI * matrix_id / NUM_INSTANCES);
+        //     float scale = 0.0004f * matrix_id;
+        //     int i = 16 * matrix_id;
+        //     mat[i + 0] = scale;
+        //     mat[i + 4] = 0.0f;
+        //     mat[i + 8] = 0.0f;
+        //     mat[i + 12] = pos_x;
+        //     mat[i + 1] = 0.0f;
+        //     mat[i + 5] = scale;
+        //     mat[i + 9] = 0.0f;
+        //     mat[i + 13] = pos_y;
+        //     mat[i + 2] = 0.0f;
+        //     mat[i + 6] = 0.0f;
+        //     mat[i + 10] = scale;
+        //     mat[i + 14] = 0.0f;
+        //     mat[i + 3] = 0.0f;
+        //     mat[i + 7] = 0.0f;
+        //     mat[i + 11] = 0.0f;
+        //     mat[i + 15] = 1.0f;
+        // }
+        glm::mat4* mat = new glm::mat4[2];
+        mat[0] = glm::translate(glm::mat4(1.0f), glm::vec3(0.5, 0, 0));
+        mat[1] = glm::translate(glm::mat4(1.0f), glm::vec3(-0.5, 0, 0));
         glGenBuffers(1, &(MBO));
         glBindBuffer(GL_ARRAY_BUFFER, MBO);
         glBufferData(GL_ARRAY_BUFFER, NUM_INSTANCES * 16 * sizeof(float), mat, GL_DYNAMIC_DRAW);
@@ -236,12 +261,32 @@ public:
             glVertexAttribDivisor(MATRICES_LOCATION + i, 1);
         }
 
+        glBindVertexArray(0);
+
         CompileShaders();
+
+        return VAO;
+    }
+
+    void Run() {
+        GLclampf Red = 0.2f, Green = 0.2f, Blue = 0.2f, Alpha = 0.0f;
+        glClearColor(Red, Green, Blue, Alpha);
+
+        // CreateVertexBuffer();
+        auto VAO_example = setupExample();
+
+        
+        // Shader shader = { "resources/shaders/instance.shader" };
+        // shader.Bind();
+        // shader.SetUniformMat4f("ProjectionView", glm::mat4(1.0));
 
         while (!glfwWindowShouldClose(window)) {
             glClear(GL_COLOR_BUFFER_BIT);
 
-            glDrawArraysInstanced(GL_TRIANGLES, 0, 3, (GLsizei) NUM_INSTANCES);
+            glUniformMatrix4fv(GetUniformLocation("ProjectionView"), 1, GL_FALSE, &matrix[0][0]);
+            glBindVertexArray(VAO_example);
+            glDrawArraysInstanced(GL_TRIANGLES, 0, 3, (GLsizei) 2);
+            glBindVertexArray(0);
 
             glfwSwapBuffers(window);
             glfwPollEvents();
