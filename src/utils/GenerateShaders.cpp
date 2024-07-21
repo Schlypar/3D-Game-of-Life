da2928c6b1649b2ac3161f358e50df89318bed27
B-den <dbancre@gmail.com>
2024-07-22 01:28:39 +0300

    satisfy Nekit: use std names that are snake case but evrthn else is camel

--- src/utils/GenerateShaders.cpp
+++ src/utils/GenerateShaders.cpp
@@ -3,64 +3,64 @@
 #include <fstream>
 #include <filesystem>
 
-const std::string disclaymer = "// DO NOT EDIT: GENERATED FILE";
+const std::string disclaimer = "// DO NOT EDIT: GENERATED FILE";
 
 int main() {
-    std::filesystem::path resources_path = "../resources";
-    std::filesystem::path hdr_path = "../resources/include";
-    std::filesystem::directory_entry resources_dir(resources_path);
+    std::filesystem::path resourcesPath = "../resources";
+    std::filesystem::path headerPath = "../resources/include";
+    std::filesystem::directory_entry resourcesDir(resourcesPath);
 
-    if (!resources_dir.exists() || !resources_dir.is_directory()) {
-        std::cerr << "failed to open dir: " << resources_path << std::endl;
+    if (!resourcesDir.exists() || !resourcesDir.is_directory()) {
+        std::cerr << "failed to open dir: " << resourcesPath << std::endl;
         return 1;
     }
 
-    if (!std::filesystem::directory_entry(hdr_path).exists()) {
-        std::filesystem::create_directory(hdr_path);
+    if (!std::filesystem::directory_entry(headerPath).exists()) {
+        std::filesystem::create_directory(headerPath);
     }
 
 
-    for (auto& dir_entry : std::filesystem::recursive_directory_iterator(resources_path)) {
-        if (!dir_entry.is_regular_file()) {
+    for (auto& dirEntry : std::filesystem::recursive_directory_iterator(resourcesPath)) {
+        if (!dirEntry.is_regular_file()) {
             // std::cerr << "skipping non regular file: " << dir_entry << std::endl;
             continue;
         }
 
-        if (!dir_entry.path().string().contains(".shader")) {
+        if (!dirEntry.path().string().contains(".shader")) {
             continue;
         }
 
-        auto out_name = dir_entry.path().string();
-        for (int i = out_name.size() - 1; i > 0; i--) {
-            if (std::string(out_name.c_str() + i) == ".shader") {
-                out_name.resize(i);
+        auto outName = dirEntry.path().string();
+        for (int i = outName.size() - 1; i > 0; i--) {
+            if (outName.substr(i) == ".shader") {
+                outName.resize(i);
                 break;
             }
         }
-        for (int i = 0; i < out_name.size(); i++) {
-            if (!std::string(out_name.c_str() + i).contains("/")) {
-                out_name = std::string(out_name.c_str() + i);
-                out_name = "shader_" + out_name;
+        for (int i = 0; i < outName.size(); i++) {
+            if (!outName.substr(i).contains("/")) {
+                outName = outName.substr(i);
+                outName = "shader_" + outName;
             }
         }
 
-        std::cout << out_name << std::endl;
+        std::cout << outName << std::endl;
 
-        auto src = std::ifstream(dir_entry.path());
+        auto src = std::ifstream(dirEntry.path());
         if (!src.is_open()) {
-            std::cerr << "failed to open file: " << dir_entry << std::endl;
+            std::cerr << "failed to open file: " << dirEntry << std::endl;
             continue;
         }
 
-        auto out = std::ofstream(hdr_path.string() + "/" + out_name + ".h", std::ios::out);
+        auto out = std::ofstream(headerPath.string() + "/" + outName + ".h", std::ios::out);
         if (!out.is_open()) {
-            std::cerr << "failed to open file for write: " << hdr_path.string() + "/" + out_name + ".h" << std::endl;
+            std::cerr << "failed to open file for write: " << headerPath.string() + "/" + outName + ".h" << std::endl;
             continue;
         }
         out <<
-            disclaymer +
+            disclaimer +
             "\n#pragma once\n#include <string>\n\nconst std::string __" +
-            out_name +
+            outName +
             " = \"\\\n";
 
         std::string line;
