26ed9cd6f2b36f3b0d3f11cb3bdf9f99d57b9112
Schlypar <schlypar@gmail.com>
2024-07-31 23:16:13 +0300

    rewrite gl buffer only on change

--- src/renderer/BatchRenderer.cpp
+++ src/renderer/BatchRenderer.cpp
@@ -30,28 +30,28 @@ void BatchRenderer::Reset() {
 }
 
 void BatchRenderer::DrawSubmitted(const Camera& camera) {
-    if (changed) {
-        this->batched = std::move(this->batcher.ComputeBatches());
-        this->changed = false;
-    }
-
     this->vertexArray.Bind();
     this->vertexBuffer.Bind();
 
-    auto modelDataSize = ranges::accumulate(this->batched, 0, [](const unsigned int size, const Surface<Vertex>& s) -> int {
-        return size + s.vertexCount * sizeof(Vertex);
-    });
-    if (modelDataSize > this->dataSize) {
-        this->vertexBuffer.Realloc(modelDataSize, GL_DYNAMIC_DRAW);
-        this->dataSize = modelDataSize;
-    }
-    unsigned int offset = 0;
-    for (Surface<Vertex>& surface : this->batched) {
-        Mesh<Vertex>* mesh = surface.mesh;
+    if (changed) {
+        this->batched = std::move(this->batcher.ComputeBatches());
+        this->changed = false;
 
-        auto& data = mesh->GetData();
-        this->vertexBuffer.Write(data.bytes, data.size, offset * sizeof(Vertex));
-        offset += surface.vertexCount;
+        auto modelDataSize = ranges::accumulate(this->batched, 0, [](const unsigned int size, const Surface<Vertex>& s) -> int {
+            return size + s.vertexCount * sizeof(Vertex);
+        });
+        if (modelDataSize > this->dataSize) {
+            this->vertexBuffer.Realloc(modelDataSize, GL_DYNAMIC_DRAW);
+            this->dataSize = modelDataSize;
+        }
+        unsigned int offset = 0;
+        for (Surface<Vertex>& surface : this->batched) {
+            Mesh<Vertex>* mesh = surface.mesh;
+
+            auto& data = mesh->GetData();
+            this->vertexBuffer.Write(data.bytes, data.size, offset * sizeof(Vertex));
+            offset += surface.vertexCount;
+        }
     }
 
     glm::mat4 modelMatrix = glm::mat4(1.0f);
